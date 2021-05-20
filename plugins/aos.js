86813539ed51b1ad5e7340372bc65721b7a40181
Khoa Doan <dntkhoa@gmail.com>
2021-05-20 04:35:30 +0200

    Fix a aos on mobile view

--- plugins/aos.js
+++ plugins/aos.js
@@ -3,7 +3,6 @@ import "aos/dist/aos.css";
 
 export default ({ app }) => {
   app.AOS = new AOS.init({ 
-    disable: window.innerWidth < 640,
     // offset: 200,
     duration: 600,
     easing: 'ease-in-out-cubic',
